b5ce175084410bb6ec94885b05079b19f74a89e1
Neel Mehta <neelmehta@college.harvard.edu>
2017-03-30 17:00:14 -0400

    update render

--- scripts/render.R
+++ scripts/render.R
@@ -1,4 +1,3 @@
-
 source('~/Documents/Git/politicalanalytics/scripts/turnout.R')
 data <- read.csv("data/L2/random-samples/UT-randsamp.csv")
 zip_data <- read.csv("data/flagships.csv")
@@ -6,22 +5,12 @@ zip_data <- read.csv("data/flagships.csv")
 ages <- seq(1:100)
 # turnouts, inner
 inner <- get_inner_zips(zip_data, "UT")
-turnouts <- unlist(lapply(ages, function(x) get_turnout_2016_by_exact_age(data, inner, x)))
-
-
-
-
-
-ages <- seq(1:100)
-# turnouts, inner
-inner <- get_inner_zips(zip_data, "UT")
 inner_turnouts <- unlist(lapply(ages, function(x) get_turnout_2016_by_exact_age(data, inner, x)))
 outer <- get_outer_zips(zip_data, "UT")
 outer_turnouts <- unlist(lapply(ages, function(x) get_turnout_2016_by_exact_age(data, outer, x)))
+frame <- data.frame(ages, inner_turnouts, outer_turnouts)
 
-# frame <- data.frame(ages, inner_turnouts, outer_turnouts)
-
-frame <- read.csv("out/turnouts.csv")
+# frame <- read.csv("out/turnouts.csv")
 library(reshape2)
 melted <- melt(frame, measure.vars = c("inner_turnouts", "outer_turnouts"))
 library(ggplot)
