25fa6feb42578b2f9a815f41eff6e19acd304dad
Neel Mehta <neelmehta@college.harvard.edu>
2017-03-30 15:31:13 -0400

    new functions

--- scripts/turnout.R
+++ scripts/turnout.R
@@ -1,5 +1,5 @@
 get_turnout <- function(data, zip_vector) {
-  in_zips <- data[data$zip %in% zips,]
+  in_zips <- data[data$zip %in% zip_vector,]
   voted <- in_zips[in_zips$turn.2016.gen == "Y",]
   turnout <- nrow(voted) / nrow(in_zips)
   return(turnout)
